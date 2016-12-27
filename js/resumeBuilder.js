51c88f6f7a90aa2678d9f3b7c149459b17f96d0f
x404 <kronos2003@gmail.com>
2016-12-27 18:46:42 +0200

    replace and append/prepend methods

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+
+var myName = 'Aleksey',
+	role = 'Web Developer';
+var awesomeThoughts = "I am Aleksey and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
+
+console.log(funThoughts);
+
+
+var formattedName = HTMLheaderName.replace('%data%', myName);
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
