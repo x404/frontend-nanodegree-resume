3b353b29752a442c16211b6577d07fb2c225737b
x404 <kronos2003@gmail.com>
2016-12-27 22:43:11 +0200

    Работа с объектами.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-
-
 var myName = 'Aleksey',
 	role = 'Web Developer';
 var awesomeThoughts = "I am Aleksey and I am AWESOME!";
@@ -16,3 +10,37 @@ var formattedName = HTMLheaderName.replace('%data%', myName);
 var formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+
+var skills = ["css", "html", "javascript", "programming", "CMS"]
+
+var bio = {
+	"name" : myName,
+	"role" : role,
+	"contacts" : {
+		"mobile" : "+380963974481",
+		"email" : "info@proverstka.com.ua",
+		"location" : "Dnepr"
+	},
+	"pic" : "/images/197x148.gif",
+	"welcomeMessage" : "Welcome to my test file",
+	"skills" : [
+		"css", "html", "javascript", "programming", "CMS"
+		]
+};
+
+
+
+// Работа с объектами
+var work = {};
+work.position = 'freelance';
+work.employer = 'proverstka';
+work.years = 10;
+
+
+var education = {};
+education['name'] = 'NMetAU';
+education['years'] = '2000-2008';
+education['city'] = 'Dnepr, UA';
+
+
+$('#main').append(work['position']).append(education.name);
