a4b26c74c733ca965eb692314a533cd36c47d46a
x404 <kronos2003@gmail.com>
2016-12-29 20:31:49 +0200

    add "if statement"

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-var myName = 'Aleksey',
+/*var myName = 'Aleksey',
 	role = 'Web Developer';
 var awesomeThoughts = "I am Aleksey and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
@@ -10,12 +10,13 @@ var formattedName = HTMLheaderName.replace('%data%', myName);
 var formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+*/
 
-var skills = ["css", "html", "javascript", "programming", "CMS"]
+// var skills = ["css", "html", "javascript", "programming", "CMS"]
 
 var bio = {
-	"name" : myName,
-	"role" : role,
+	"name" : "Aleksey",
+	"role" : "Web Developer",
 	"contacts" : {
 		"mobile" : "+380963974481",
 		"email" : "info@proverstka.com.ua",
@@ -31,16 +32,79 @@ var bio = {
 
 
 // Работа с объектами
-var work = {};
-work.position = 'freelance';
-work.employer = 'proverstka';
-work.years = 10;
+var work = {
+	"jobs" : [
+		{
+			"position" : "freelance",
+			"employer" : "proverstka",
+			"years" : 10
+		},
+		{
+			"position" : "developer",
+			"employer" : "promarmatura",
+			"years" : 4
+		}
+	]
+};
 
 
-var education = {};
-education['name'] = 'NMetAU';
-education['years'] = '2000-2008';
-education['city'] = 'Dnepr, UA';
+var projects = {
+	"projects" : [
+		{
+			"title" : "Project1",
+			"dates" : "2010",
+			"description" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium."
+		}
+	]	
+}
 
 
-$('#main').append(work['position']).append(education.name);
+var education = {
+	"school" : [
+		{
+			"name" : "College",
+			"city" : "Dnepr",
+			"majors" : ["CS"],
+			"minors" : "rea",
+			"years" : "1996-2000"
+		},
+		{
+			"name" : "Academy",
+			"city" : "Dnepr",
+			"majors" : ["HT"],
+			"minors" : "metal",
+			"years" : "2000-2008"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "JS Courses",
+			"school" : "Udacity",
+			"dates" : 2016
+		}
+	]
+}
+
+// console.log(work.jobs[0].position);
+
+// $('#main').append(work.jobs[0].position).append(education.school[0].name);
+
+
+// console.log(document.getElementsByClassName("text-atom")[0]);
+
+if (bio.skills.length > 0){
+	$('#header').append(HTMLskillsStart);
+	// console.log(HTMLskillsStart);
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$('#skills').append(formattedSkill);
+
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$('#skills').append(formattedSkill);
+
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$('#skills').append(formattedSkill);
+
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	$('#skills').append(formattedSkill);
+}
+
